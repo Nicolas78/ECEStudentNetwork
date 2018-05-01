60a7db486eacbf2b4fda553b67692eb03b84361b
DESKTOP-2HL8IOU\Léo <leo.remurier@gmail.com>
2018-05-01 16:47:05 +0200

    Maj bdd

--- bdd et sauvegarde/bdd.sql
+++ bdd et sauvegarde/bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 01 mai 2018 à 14:42
+-- Généré le :  mar. 01 mai 2018 à 14:46
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
