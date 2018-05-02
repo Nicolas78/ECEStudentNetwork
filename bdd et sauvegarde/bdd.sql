5b0db02ceaee54dc3948de962c8613b10ba62719
Nicolas33333 <Nicolas33333@users.noreply.github.com>
2018-05-02 11:57:29 +0200

    BDD finale et ajout d'images

--- bdd et sauvegarde/bdd.sql
+++ bdd et sauvegarde/bdd.sql
@@ -1,4 +1,6 @@
-﻿--
+
+
+--
 -- Base de données :  `bdd`
 --
 
@@ -11,13 +13,21 @@
 DROP TABLE IF EXISTS `aime`;
 CREATE TABLE IF NOT EXISTS `aime` (
   `id_aime` int(4) NOT NULL,
-  `date_aime` date NOT NULL,
+  `date_aime` datetime NOT NULL,
   `id_utilisateur` int(4) NOT NULL,
   `id_publication` int(4) NOT NULL,
   PRIMARY KEY (`id_aime`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_publication` (`id_publication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `aime`
+--
+
+INSERT INTO `aime` (`id_aime`, `date_aime`, `id_utilisateur`, `id_publication`) VALUES
+(1, '2018-05-08 07:23:23', 6, 1),
+(2, '2018-05-02 15:17:24', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -36,6 +46,14 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
   KEY `id_publication` (`id_publication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `commentaire`
+--
+
+INSERT INTO `commentaire` (`id_commentaire`, `contenu`, `id_publication`, `id_utilisateur`) VALUES
+(1, 'ceci est un commentaire', 1, 9),
+(2, 'Ceci est aussi un commentaire', 1, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +71,23 @@ CREATE TABLE IF NOT EXISTS `contact` (
   KEY `id_utilisateur2` (`id_utilisateur2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id_contact`, `type`, `id_utilisateur1`, `id_utilisateur2`) VALUES
+(1, 'ami', 6, 3),
+(2, 'contact professionel', 1, 5),
+(3, 'ami', 7, 2),
+(4, 'contact professionel', 1, 4),
+(5, 'ami', 7, 9),
+(6, 'contact professionel', 4, 8),
+(7, 'contact professionel', 7, 9),
+(8, 'contact professionel', 1, 4),
+(9, 'ami', 5, 10),
+(10, 'ami', 5, 6),
+(11, 'ami', 3, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -62,13 +97,25 @@ CREATE TABLE IF NOT EXISTS `contact` (
 DROP TABLE IF EXISTS `conversation`;
 CREATE TABLE IF NOT EXISTS `conversation` (
   `id_conversation` int(4) NOT NULL,
-  `nom` varchar(30) NOT NULL,
+  `nom_conversation` varchar(30) NOT NULL,
   `id_utilisateur1` int(4) NOT NULL,
   `id_utilisateur2` int(4) NOT NULL,
   PRIMARY KEY (`id_conversation`),
   KEY `id_utilisateur1` (`id_utilisateur1`),
   KEY `id_utilisateur2` (`id_utilisateur2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `conversation`
+--
+
+INSERT INTO `conversation` (`id_conversation`, `nom_conversation`, `id_utilisateur1`, `id_utilisateur2`) VALUES
+(1, 'Conv1', 5, 9),
+(2, 'Conv2', 8, 3),
+(3, 'Conv2', 6, 4),
+(4, 'Conv3', 8, 2),
+(5, 'Conv5', 1, 2),
+(6, 'Conv6', 4, 3);
 
 -- --------------------------------------------------------
 
@@ -79,8 +126,7 @@ CREATE TABLE IF NOT EXISTS `conversation` (
 DROP TABLE IF EXISTS `emploi`;
 CREATE TABLE IF NOT EXISTS `emploi` (
   `id_emploi` int(4) NOT NULL,
-  `entreprise` varchar(20) NOT NULL,
-  `periode` int(20) NOT NULL,
+  `type_emploi` enum('CDD','CDI','STAGE') NOT NULL,
   `actions` varchar(20) NOT NULL,
   `id_auteur` int(4) NOT NULL,
   `id_entreprise` int(4) NOT NULL,
@@ -88,6 +134,17 @@ CREATE TABLE IF NOT EXISTS `emploi` (
   KEY `id_auteur` (`id_auteur`),
   KEY `id_entreprise` (`id_entreprise`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `emploi`
+--
+
+INSERT INTO `emploi` (`id_emploi`, `type_emploi`, `actions`, `id_auteur`, `id_entreprise`) VALUES
+(1, 'CDI', 'Gestions des projets', 1, 1),
+(2, 'CDI', 'assistants', 4, 1),
+(3, 'CDD', 'Chef de projet', 7, 4),
+(4, 'STAGE', 'stage communication', 10, 3),
+(5, 'CDD', 'Respo Ventes', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -100,10 +157,20 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `id_entreprise` int(4) NOT NULL,
   `nom_entreprise` varchar(20) NOT NULL,
   `secteur_activite` varchar(20) NOT NULL,
-  `logo` int(4) NOT NULL,
+  `logo` varchar(30) NOT NULL,
   `mail_entreprise` varchar(20) NOT NULL,
   PRIMARY KEY (`id_entreprise`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `entreprise`
+--
+
+INSERT INTO `entreprise` (`id_entreprise`, `nom_entreprise`, `secteur_activite`, `logo`, `mail_entreprise`) VALUES
+(1, 'ENCOM', 'Numérique', '1.jpg', 'encom@gmail.com'),
+(2, 'Logee', 'Design', '2.jpg', 'libee@yahoo.com'),
+(3, 'Robotics', 'Robotique', '3.jpg', 'robotics@outlook.com'),
+(4, 'BLN', 'Numérique', '4.jpg', 'bln@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -115,7 +182,8 @@ DROP TABLE IF EXISTS `media`;
 CREATE TABLE IF NOT EXISTS `media` (
   `id_media` int(4) NOT NULL,
   `lieu` varchar(20) NOT NULL,
-  `date_media` date NOT NULL,
+  `fichier` varchar(30) NOT NULL,
+  `date_media` datetime NOT NULL,
   `nbreLike` int(4) NOT NULL,
   `nbreCommentaire` int(4) NOT NULL,
   `nbrePartage` int(4) NOT NULL,
@@ -123,6 +191,13 @@ CREATE TABLE IF NOT EXISTS `media` (
   PRIMARY KEY (`id_media`),
   KEY `id_utilisateur` (`id_utilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `media`
+--
+
+INSERT INTO `media` (`id_media`, `lieu`, `fichier`, `date_media`, `nbreLike`, `nbreCommentaire`, `nbrePartage`, `id_utilisateur`) VALUES
+(1, 'Paris', 'remise_diplomes.jpg', '2018-05-02 07:26:22', 0, 0, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -140,6 +215,14 @@ CREATE TABLE IF NOT EXISTS `message` (
   KEY `id_conversation` (`id_conversation`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `message`
+--
+
+INSERT INTO `message` (`id_message`, `date_message`, `contenu`, `id_conversation`) VALUES
+(1, '2018-04-04 06:30:23', 'test test test', 2),
+(2, '2018-05-23 10:29:28', 'test2 test2', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -149,15 +232,23 @@ CREATE TABLE IF NOT EXISTS `message` (
 DROP TABLE IF EXISTS `notification`;
 CREATE TABLE IF NOT EXISTS `notification` (
   `id_notification` int(4) NOT NULL,
-  `nom` varchar(12) NOT NULL,
+  `nom` enum('DEMANDE D''AMI','DEMANDE DE CONNAISSANCE PRO','NOUVELLE PUBLICATION','NOUVEAU MEDIA') NOT NULL,
   `contenu` varchar(50) NOT NULL,
-  `date_notif` date NOT NULL,
-  `id_contact` int(4) NOT NULL,
-  `id_publication` int(4) NOT NULL,
+  `date_notif` datetime NOT NULL,
+  `id_contact` int(4) DEFAULT NULL,
+  `id_publication` int(4) DEFAULT NULL,
   PRIMARY KEY (`id_notification`),
   KEY `id_contact` (`id_contact`),
   KEY `id_publication` (`id_publication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `notification`
+--
+
+INSERT INTO `notification` (`id_notification`, `nom`, `contenu`, `date_notif`, `id_contact`, `id_publication`) VALUES
+(1, 'DEMANDE D\'AMI', 'Nouvelle demande d\'ami', '2018-05-02 14:18:00', 9, NULL),
+(2, 'NOUVELLE PUBLICATION', 'nouvelle publication de Mr Saumon', '2018-04-19 00:28:00', 7, 2);
 
 -- --------------------------------------------------------
 
@@ -168,13 +259,22 @@ CREATE TABLE IF NOT EXISTS `notification` (
 DROP TABLE IF EXISTS `partage`;
 CREATE TABLE IF NOT EXISTS `partage` (
   `id_partage` int(4) NOT NULL,
-  `date_partage` date NOT NULL,
+  `date_partage` datetime NOT NULL,
   `id_utilisateur` int(4) NOT NULL,
   `id_publication` int(4) NOT NULL,
   PRIMARY KEY (`id_partage`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_publication` (`id_publication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `partage`
+--
+
+INSERT INTO `partage` (`id_partage`, `date_partage`, `id_utilisateur`, `id_publication`) VALUES
+(1, '2018-05-02 06:25:22', 9, 2),
+(2, '2018-04-11 00:00:00', 6, 1),
+(3, '2018-05-01 00:23:38', 5, 2);
 
 -- --------------------------------------------------------
 
@@ -185,18 +285,26 @@ CREATE TABLE IF NOT EXISTS `partage` (
 DROP TABLE IF EXISTS `publication`;
 CREATE TABLE IF NOT EXISTS `publication` (
   `id_publication` int(4) NOT NULL,
-  `date_publication` date NOT NULL,
+  `date_publication` datetime NOT NULL,
   `contenu` varchar(50) NOT NULL,
   `titre_publication` varchar(20) NOT NULL,
   `nbreLike` int(4) NOT NULL,
   `nbreCommentaire` int(4) NOT NULL,
   `nbrePartage` int(4) NOT NULL,
   `id_utilisateur` int(4) NOT NULL,
-  `id_media` int(4) NOT NULL,
+  `id_media` int(4) DEFAULT NULL,
   PRIMARY KEY (`id_publication`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_media` (`id_media`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `publication`
+--
+
+INSERT INTO `publication` (`id_publication`, `date_publication`, `contenu`, `titre_publication`, `nbreLike`, `nbreCommentaire`, `nbrePartage`, `id_utilisateur`, `id_media`) VALUES
+(1, '2018-05-02 07:27:17', 'blabla', 'Remise de diplome', 0, 0, 0, 10, 1),
+(2, '2018-04-11 11:23:00', 'Nouveau travail', 'Publication ', 0, 0, 0, 6, NULL);
 
 -- --------------------------------------------------------
 
@@ -227,16 +335,16 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`id_utilisateur`, `nom`, `prenom`, `mail`, `motdepasse`, `lastConnexion`, `promotion`, `type`, `interet`, `photo`, `CV`, `imagefond`, `humeur`) VALUES
-(1, 'Nicolas', 'Baralle', 'nicolas.baralle@edu.ece.fr', 'nicolas', '2018-05-01', 2020, 'Auteur', 'Java', NULL, NULL, NULL, 'CONTENT'),
-(2, 'Poletto', 'Mathieu', 'mathieu.poletto@edu.ece.fr', 'Mathieu', '2018-05-01', 2020, 'Administrateur', 'Sport', NULL, NULL, NULL, 'CONTENT'),
-(3, 'Remurier', 'Leo', 'leo.remurier@edu.ece.fr', 'Leo', '2018-05-01', 2020, 'Auteur', 'Sport', NULL, NULL, NULL, 'CONTENT'),
-(4, 'Saumon', 'Jean', 'jean.saumon@gmail.com', 'Jean', '2018-05-01', 2020, 'Auteur', 'Java', NULL, NULL, NULL, 'PAS CONTENT'),
-(5, 'Carpe', 'Pierre', 'pierre.carpe@gmail.com', 'Pierre', '2018-05-01', 2020, 'Auteur', 'Sciences', NULL, NULL, NULL, 'BOF'),
-(6, 'Merou', 'Didier', 'didier.meru@gmail.com', 'Didier', '2018-05-01', 2020, 'Auteur', 'Art', NULL, NULL, NULL, 'CONTENT'),
-(7, 'Bar', 'Christophe', 'christophe.bar@gmail.com', 'Christophe', '2018-05-01', 2020, 'Auteur', 'C++', NULL, NULL, NULL, 'CONTENT'),
-(8, 'Truite', 'Claude', 'claude.truite@gmail.com', 'Claude', '2018-05-01', 2020, 'Auteur', 'Python', NULL, NULL, NULL, 'CONTENT'),
-(9, 'Merlu', 'Clement', 'clement.merlu@gmail.com', 'Clement', '2018-05-01', 2020, 'Auteur', 'C', NULL, NULL, NULL, 'CONTENT'),
-(10, 'Requin', 'Charles', 'charles.requin@gmail.com', 'Charles', '2018-05-01', 2020, 'Auteur', 'Matlab', NULL, NULL, NULL, 'CONTENT');
+(1, 'Nicolas', 'Baralle', 'nicolas.baralle@edu.ece.fr', 'nicolas', '2018-05-01', 2020, 'Auteur', 'Java', 'profil1.jpg', NULL, 'fond1.jpg', 'CONTENT'),
+(2, 'Poletto', 'Mathieu', 'mathieu.poletto@edu.ece.fr', 'Mathieu', '2018-05-01', 2020, 'Administrateur', 'Sport', 'profil2.jpg', NULL, 'fond2.jpg', 'CONTENT'),
+(3, 'Remurier', 'Leo', 'leo.remurier@edu.ece.fr', 'Leo', '2018-05-01', 2020, 'Auteur', 'Sport', 'profil3.jpg', NULL, 'fond3.jpg', 'CONTENT'),
+(4, 'Saumon', 'Jean', 'jean.saumon@gmail.com', 'Jean', '2018-05-01', 2022, 'Auteur', 'Java', 'profil4.jpg', NULL, 'fond4.jpg', 'PAS CONTENT'),
+(5, 'Carpe', 'Pierre', 'pierre.carpe@gmail.com', 'Pierre', '2018-05-01', 2020, 'Auteur', 'Sciences', 'profil5.jpg', NULL, 'fond5.jpg', 'BOF'),
+(6, 'Merou', 'Didier', 'didier.meru@gmail.com', 'Didier', '2018-05-01', 2020, 'Auteur', 'Art', 'profil6.jpg', NULL, 'fond6.jpg', 'CONTENT'),
+(7, 'Bar', 'Christophe', 'christophe.bar@gmail.com', 'Christophe', '2018-05-01', 2020, 'Auteur', 'C++', 'profil7.jpg', NULL, 'fond7.jpg', 'CONTENT'),
+(8, 'Truite', 'Claude', 'claude.truite@gmail.com', 'Claude', '2018-05-01', 2020, 'Auteur', 'Python', 'profil8.jpg', NULL, 'fond8.jpg', 'CONTENT'),
+(9, 'Merlu', 'Clement', 'clement.merlu@gmail.com', 'Clement', '2018-05-01', 2018, 'Auteur', 'C', 'profil9.jpg', NULL, 'fond9.jpg', 'CONTENT'),
+(10, 'Requin', 'Charles', 'charles.requin@gmail.com', 'Charles', '2018-05-01', 2021, 'Auteur', 'Matlab', 'profil10.jpg', NULL, 'fond10.jpg', 'CONTENT');
 
 --
 -- Contraintes pour les tables déchargées
