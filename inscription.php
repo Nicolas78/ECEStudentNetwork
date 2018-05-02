29b78b2964034a407dd5bcf999f862bd318bb820
DESKTOP-2HL8IOU\Léo <leo.remurier@gmail.com>
2018-05-02 09:37:26 +0200

    Inscription maj

--- inscription.php
+++ inscription.php
@@ -16,35 +16,36 @@ $db_found = mysqli_select_db($db_handle,$database); //connexion à la BDD
 
 if($inscrire){ //si bouton clické
 
-if ($Prenom != "" && $Nom != "" &&  $Email != "" && $Motdepasse != "" && !empty($photo_de_profil) && !empty($image_de_fond)) {
-
-	/*if($db_found)
-	{
-		$sql1 = "SELECT * FROM bestenterprisetbl WHERE bestenterprisetbl.Prenom = '".prenom."' ";
-		$result1 = mysqli_query($db_handle,$sql1);
-	
-	}
-
-	$sql = 	
-
-	$result1 = mysqli_query($db_handle,$sql);
-
-	while($data = mysqli_fetch_row($result1))
-	{
-		echo "Prenom:" .$data[0] ;echo "<br>";
-			
-	}
-
-	$result = mysqli_query($db_handle,$sql);*/
-	echo "Add successful";
-}
-	
-else //échec connexion à la BDD
+if ($Prenom != "" && $Nom != "" &&  $Email != "" && $Motdepasse != "" )
 {
-	echo "Database not found";
+
+	if($db_found)
+	{
+		echo "echo3";
+
+		$sql = "INSERT INTO utilisateur(nom, prenom, mail, motdepasse) VALUES('$Prenom','$Nom','$Email','$Motdepasse')";
+
+		$requete = mysqli_query($db_handle,$sql);
+
+		echo "echo4";
+
+		if ($requete) 
+		{
+			echo "Add successful";
+		}
+
+		else
+		{
+			echo "Add failed";
+		}
 }
+	
+	else //échec connexion à la BDD
+	{
+		echo "Database not found";
+	}
 }
 
-//mysqli_close($db_handle);
+}
 
- ?>
+?>
